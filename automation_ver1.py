4b7b6691c52f728e24a6441cb2a4a7d3a86ea575
HanGwonin_Kenji <63086285+gwonin-kenji@users.noreply.github.com>
2021-05-01 10:16:30 +0900

    gwonin merged first commit to develop without pull request. Sorry, I didn't follow the rule of 'merge'.

--- automation_ver1.py
+++ automation_ver1.py
@@ -11,12 +11,3 @@ import time
 import pyperclip
 from datetime import datetime, date 
 import datetime 
-
-
-# gwonin branch  
-def insert_due_date():
-    pass
-
-# gwonin branch 
-def insert_due_time():
-    pass
